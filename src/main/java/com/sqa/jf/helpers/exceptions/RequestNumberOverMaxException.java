00ac2536711a66f7f98f2d96831750b3108fd592
Jean-francois Nepton <sqasolution@hotmail.com>
2017-10-07 17:06:27 -0700

    Update BasicApp with Request Methods

--- src/main/java/com/sqa/jf/helpers/exceptions/RequestNumberOverMaxException.java
+++ src/main/java/com/sqa/jf/helpers/exceptions/RequestNumberOverMaxException.java
@@ -1,24 +1,27 @@
 /**
- * File Name: RequestNumberOverMaxException.java<br>
- * Nepton, Jean-francois<br>
- * Java Boot Camp Exercise<br>
- * Instructor: Jean-francois Nepton<br>
- * Created: Oct 5, 2017
+ *   File Name: RequestIntOverMaxException.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Oct 7, 2017
+ *   
  */
+
 package com.sqa.jf.helpers.exceptions;
 
-import com.sqa.jf.helpers.exceptions.*;
 
 /**
- * RequestNumberOverMaxException //ADDD (description of class)
+ * RequestIntOverMaxException //ADDD (description of class)
  * <p>
  * //ADDD (description of core fields)
  * <p>
  * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
  *
- * @author Nepton, Jean-francois
- * @version 1.0.0
- * @since 1.0
  */
-public class RequestNumberOverMaxException extends RequestNumberNotWithinRangeException {
+public class RequestNumberOverMaxException extends Exception {
 }
--- src/main/java/com/sqa/jf/helpers/exceptions/RequestNumberUnderMinException.java
+++ src/main/java/com/sqa/jf/helpers/exceptions/RequestNumberUnderMinException.java
@@ -1,22 +1,27 @@
 /**
- * File Name: RequestNumberUnderMinException.java<br>
- * Nepton, Jean-francois<br>
- * Java Boot Camp Exercise<br>
- * Instructor: Jean-francois Nepton<br>
- * Created: Oct 5, 2017
+ *   File Name: RequestIntUnderMinException.java<br>
+ *
+ *   Nepton, Jean-francois<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Oct 7, 2017
+ *   
  */
+
 package com.sqa.jf.helpers.exceptions;
 
+
 /**
- * RequestNumberUnderMinException //ADDD (description of class)
+ * RequestIntUnderMinException //ADDD (description of class)
  * <p>
  * //ADDD (description of core fields)
  * <p>
  * //ADDD (description of core methods)
+ * 
+ * @author      Nepton, Jean-francois
+ * @version     1.0.0
+ * @since       1.0
  *
- * @author Nepton, Jean-francois
- * @version 1.0.0
- * @since 1.0
  */
-public class RequestNumberUnderMinException extends RequestNumberNotWithinRangeException {
+public class RequestNumberUnderMinException extends Exception {
 }
